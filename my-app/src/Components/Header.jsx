e37cf5c20951a9e8a0cde781e799ae54edf516e5
Sachin Khati <khatisachin448@gmail.com>
2025-04-22 13:20:48 +0530

    added taikwind

--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -3,38 +3,21 @@ import React from 'react'
 function Header() {
   return (
     <div>
-       <header className="bg-orange-600 text-white p-4 shadow-md">
+       <header className="bg-white text-orange-600 p-4 shadow-md">
         <div className="container mx-auto flex justify-between items-center">
           <div className="flex items-center space-x-2">
             {/* <Map size={28} /> */}
-            <h1 className="text-2xl font-bold">TrafficEase</h1>
+            <h1 className="text-3xl font-bold pl-14  ">Mandeer Traffic Guide </h1>
           </div>
           <nav>
             <ul className="flex space-x-6">
-              <li className="cursor-pointer hover:underline">Dashboard</li>
-              <li className="cursor-pointer hover:underline">Live Map</li>
-              <li className="cursor-pointer hover:underline">Route Planner</li>
-              <li className="cursor-pointer hover:underline">About</li>
+              <li className="cursor-pointer hover:underline font-bold text-xl text-black ">Current Traffic</li>
+              <li className="cursor-pointer hover:underline font-semibold">Medium</li>
             </ul>
           </nav>
         </div>
       </header>
-      <header className="bg-orange-600 text-white p-4 shadow-md">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="flex items-center space-x-2">
-            {/* <Map size={28} /> */}
-            <h1 className="text-2xl font-bold">TrafficEase</h1>
-          </div>
-          <nav>
-            <ul className="flex space-x-6">
-              <li className="cursor-pointer hover:underline">Dashboard</li>
-              <li className="cursor-pointer hover:underline">Live Map</li>
-              <li className="cursor-pointer hover:underline">Route Planner</li>
-              <li className="cursor-pointer hover:underline">About</li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+     
     </div>
   )
 }
